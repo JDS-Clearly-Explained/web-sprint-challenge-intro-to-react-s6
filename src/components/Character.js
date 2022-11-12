b6485c8a10e755e2095767c7442eb70d622bfbe0
Dionne <mylife33@hotmail.com>
2022-11-11 22:58:58 -0600

    mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+
+import React from "react";
+
+export default function Character(props) {
+  const { name, height } = props.character;
+  console.log(props);
+  return (
+    <div>
+      <p>{name}</p>
+      <p>Height: {height}</p>
+    </div>
+  );
+}
